a16eb16433cb355b909e55c9e5111bb3ed516c5a
ale0726 <97697256+ale0726@users.noreply.github.com>
2022-04-26 20:02:29 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyApRGksHtmPmelQYyBll0UCnT-H46yRx5I",
+    authDomain: "storyapp-7a6c7.firebaseapp.com",
+    projectId: "storyapp-7a6c7",
+    storageBucket: "storyapp-7a6c7.appspot.com",
+    messagingSenderId: "154404513758",
+    appId: "1:154404513758:web:cf4bfff0dafb9d4ec89f4f"
+  };
